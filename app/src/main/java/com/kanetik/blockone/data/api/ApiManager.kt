817a17f57a9405993dc23a62187341d5405550ee
Jeremy Kane <jkane001@gmail.com>
2018-09-22 13:26:16 -0400

    Preparing for Retrofit

---
+++ app/src/main/java/com/kanetik/blockone/data/api/ApiManager.kt
@@ -0,0 +1,18 @@
+package com.kanetik.blockone.data.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+object ApiManager {
+    private const val ENDPOINT_BASE = "https://api.eosnewyork.io/v1"
+
+    fun <T> getService(service: Class<T>): T {
+        val retrofitBuilder = Retrofit.Builder()
+                .client(OkHttpClient.Builder().build())
+                .baseUrl(ENDPOINT_BASE)
+                .addConverterFactory(MoshiConverterFactory.create())
+
+        return retrofitBuilder.build().create(service)
+    }
+}
